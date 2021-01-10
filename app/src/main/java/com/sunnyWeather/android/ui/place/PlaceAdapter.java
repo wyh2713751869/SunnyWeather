f210f0707672d81b8100e129179ce0faadaa5b24
wyh2713751869 <2713751869@qq.com>
2021-01-10 20:13:56 +0800

    加入显示天气信息的功能。

--- app/src/main/java/com/sunnyWeather/android/ui/place/PlaceAdapter.java
+++ app/src/main/java/com/sunnyWeather/android/ui/place/PlaceAdapter.java
@@ -7,7 +7,9 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.sunnyWeather.android.ui.weather.WeatherActivity;
@@ -17,10 +19,11 @@ import com.sunnyWeather.android.logic.model.Place;
 import java.util.List;
 
 public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.ViewHolder> {
-    Fragment fragment;
+
+    PlaceFragment fragment;
     List<Place> placeList;
 
-    public PlaceAdapter(Fragment fragment, List<Place> placeList) {
+    public PlaceAdapter(PlaceFragment fragment, List<Place> placeList) {
         this.fragment = fragment;
         this.placeList = placeList;
     }
@@ -31,15 +34,28 @@ public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.ViewHolder> 
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.place_item, parent, false);
         ViewHolder viewHolder = new ViewHolder(view);
         viewHolder.itemView.setOnClickListener(v -> {
+
             int adapterPosition = viewHolder.getAdapterPosition();
             Place place = placeList.get(adapterPosition);
 
-            Intent intent = new Intent(parent.getContext(), WeatherActivity.class);
-            intent.putExtra("location_lng",place.getLocation().getLng());
-            intent.putExtra("location_lat",place.getLocation().getLat());
-            intent.putExtra("place_name",place.getName());
-
-            fragment.startActivity(intent);
+            FragmentActivity activity = fragment.getActivity();
+            if (activity instanceof WeatherActivity){
+                DrawerLayout drawerLayout = activity.findViewById(R.id.drawerLayout);
+                drawerLayout.closeDrawers();
+                ((WeatherActivity) activity).getViewModel().setLocationLng(place.getLocation().getLng());
+                ((WeatherActivity) activity).getViewModel().setLocationLat(place.getLocation().getLat());
+                ((WeatherActivity) activity).getViewModel().setPlaceName(place.getName());
+                ((WeatherActivity) activity).refreshWeather();
+            }else {
+                Intent intent = new Intent(parent.getContext(), WeatherActivity.class);
+                intent.putExtra("location_lng",place.getLocation().getLng());
+                intent.putExtra("location_lat",place.getLocation().getLat());
+                intent.putExtra("place_name",place.getName());
+                fragment.getViewModel().savePlace(place);
+                fragment.startActivity(intent);
+                fragment.getActivity().finish();
+            }
+            fragment.getViewModel().savePlace(place);
         });
         return viewHolder;
     }
