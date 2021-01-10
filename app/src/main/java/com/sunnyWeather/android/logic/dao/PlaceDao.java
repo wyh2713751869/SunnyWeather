c9aaab970c82225ef58366c19d1435c93df71446
wyh2713751869 <2713751869@qq.com>
2021-01-10 16:55:35 +0800

    实现搜索全球城市数据功能。

---
+++ app/src/main/java/com/sunnyWeather/android/logic/dao/PlaceDao.java
@@ -0,0 +1,11 @@
+package com.sunnyWeather.android.logic.dao;
+
+import com.sunnyWeather.android.logic.model.Place;
+
+public class PlaceDao {
+    void savePlace(Place place)
+
+    {
+        
+    }
+}
